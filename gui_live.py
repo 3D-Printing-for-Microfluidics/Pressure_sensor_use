650062b4c2eb91ac4b68e312fd3014d7140e9ab7
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2020-05-15 18:22:49 -0600

    Add button callback functions

--- gui_live.py
+++ gui_live.py
@@ -34,12 +34,12 @@ def psi(counts, sensor='A', adc=0):
 
 # set up live plot
 fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
+ax = fig.add_subplot(1, 1, 1)
 
-def animate(_):
+def animate(_, n):
     try:
         # grab some data
-        graph_data = open('test_out.csv', 'r').read()
+        graph_data = open('test_out_static.csv', 'r').read()
         lines = graph_data.split('\n')
         ts = []
         adc0 = []
@@ -52,19 +52,20 @@ def animate(_):
                 adc1.append(psi(float(v1)))
 
         # plot last n datapoints
-        n = 50
-        ax1.clear()
-        ax1.plot(ts[-n:], adc0[-n:], label='adc0')
-        ax1.plot(ts[-n:], adc1[-n:], label='adc1')
-        ax1.set_title('Live Sensor Readings')
-        ax1.set_xlabel('Time (ms)')
-        ax1.set_ylabel('Pressure (psi)')
-        ax1.legend(loc='lower right')
+        ax.clear()
+        ax.plot(ts[-n:], adc0[-n:], label='adc0')
+        ax.plot(ts[-n:], adc1[-n:], label='adc1')
+        ax.set_title('Live Sensor Readings')
+        ax.set_xlabel('Time (ms)')
+        ax.set_ylabel('Pressure (psi)')
+        ax.legend(loc='lower right')
         # fig.tight_layout()
     except FileNotFoundError:
         exit()
 
-ani = animation.FuncAnimation(fig, animate, interval=500)
+
+window_size = 100
+ani = animation.FuncAnimation(fig, animate, interval=500, fargs=(window_size,))
 plt.draw()  # must call plot.draw() to start the animation
 
 # helper function to pass matplotlib backend to pySimpleGui canvas
@@ -77,23 +78,32 @@ def draw_figure(canvas, figure):
 
 #################### Normal PySimpleGUI Stuff ##########################
 
-sensors = ['A', 'B', 'C']
-
-layout = [[sg.Text('Window Size (ms):'),
-           sg.Input(key='-IN-', size=(5, 0), default_text=100),
-           sg.Button('Start'),
-           sg.Button('Stop'),
-           sg.Button('Save'),
-           sg.FileSaveAs(),
-           sg.Button('Exit')],
-          [sg.Canvas(key='-CANVAS-')],  # the animation
-          [sg.Frame(title='ADC 0', relief=sg.RELIEF_SUNKEN,
-                    layout=[[sg.Checkbox('Enabled', default=True)],
-                            [sg.Radio('Sensor {}'.format(t), 1) for t in sensors]]),
-           sg.Frame(title='ADC 1', relief=sg.RELIEF_SUNKEN,
-                    layout=[[sg.Checkbox('Enabled', default=True)],
-                            [sg.Radio('Sensor {}'.format(t), 2) for t in sensors]])]
+layout = [
+    [   # row 1
+        sg.Text('Window Size (ms):'),
+        sg.Input(size=(5, 0), default_text=100),
+        sg.Button('Start'),
+        sg.Button('Stop'),
+        sg.Button('Save'),
+        sg.FileSaveAs(),
+        sg.Button('Exit')
+    ],
+    [   # row 2, the animation
+        sg.Canvas(key='-CANVAS-')
+    ],
+    [  # row 3, some frames for the ADC options
+        sg.Frame(title='ADC 0', relief=sg.RELIEF_SUNKEN,
+                 layout=[[sg.Checkbox('Enabled', default=True)],
+                         [sg.Radio('Sensor A', 1, default=True),
+                          sg.Radio('Sensor B', 1),
+                          sg.Radio('Sensor C', 1)]]),
+        sg.Frame(title='ADC 1', relief=sg.RELIEF_SUNKEN,
+                 layout=[[sg.Checkbox('Enabled', default=True)],
+                         [sg.Radio('Sensor A', 2),
+                          sg.Radio('Sensor B', 2, default=True),
+                          sg.Radio('Sensor C', 2)]])
     ]
+]
 
 # create the form and show it
 window = sg.Window('Pressure Sensors', layout, finalize=True, element_justification='center', font='18')
@@ -101,6 +111,42 @@ window = sg.Window('Pressure Sensors', layout, finalize=True, element_justificat
 # add the plot to the window
 fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
-event, values = window.read()
+
+# button callback functions
+def start_callback():
+    print('start')
+def stop_callback():
+    print('stop')
+def save_callback():
+    print('save')
+def saveas_callback():
+    print('save as')
+
+# lookup dictionary that maps buttons to callbacks
+callbacks = {
+    'Start': start_callback,
+    'Stop': stop_callback,
+    'Save': save_callback,
+    'Save As...': saveas_callback,
+}
+
+# main event loop
+while True:
+
+    event, values = window.read()
+    if event in ('Exit', None):
+        break
+
+    # lookup event in function dictionary
+    if event in callbacks:
+        func_to_call = callbacks[event]   # get function from callback dictionary
+        func_to_call()
+    else:
+        print('Event {} not in dispatch dictionary'.format(event))
+
+    print(values)
+    print(values[0])
+    print('ADC0: enabled:{}, {} {} {}'.format(values[1], values[2], values[3], values[4]))
+    print('ADC1: enabled:{}, {} {} {}'.format(values[5], values[6], values[7], values[8]))
 
 window.close()
