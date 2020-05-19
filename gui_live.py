6b7898b6b9833fdf072507740ecbfc09628103e9
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2020-05-18 18:03:26 -0600

    Fix bug in UI queue for multithreading

--- gui_live.py
+++ gui_live.py
@@ -35,63 +35,65 @@ def psi(counts, sensor='A', adc=0):
     coef = get_coef(sensor=sensor, adc=adc)
     return float(np.polyval(coef, counts))
 
-def data_collection_thread(data_queue):
+def data_collection_thread(data_queue, falsify=False):
+    if falsify: # simulate some data for development
+        t = 0
+        while True:
+            t += 1
+            x = np.sin(np.pi*t/112)*12000-10000
+            y = random.randrange(-23000, 3000)
+            line = '{},{},{}'.format(t, x, y)
+            data_queue.put(line)  # put a message into queue for GUI
+            time.sleep(0.001)   # sleep about 1ms
+    else:
+        # ser.read() will go here
+        pass
 
-    # for now, just simulate some random data for development
-    t = 0
-    while True:
-        t += 1
-        x = random.randrange(-25000, 5000)
-        y = random.randrange(-25000, 5000)
-        line = '{},{},{}'.format(t, x, y)
-        data_queue.put(line)  # put a message into queue for GUI
-        time.sleep(0.001)   # sleep about 1ms
+# process all data on queue
+def process_data(data_queue, t, x, y):
+    while not data_queue.empty():
+        line = data_queue.get()
+        t0, v0, v1 = line.split(',')
+        t.append(float(t0))
+        x.append(psi(float(v0)))
+        y.append(psi(float(v1)))
+        data_queue.task_done()
+    return t[-window_size:], x[-window_size:], y[-window_size:]
 
 # set up live plot
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ui_queue = queue.Queue()
 data_collection_queue = queue.Queue()
+update_rate_ms = 50 # refresh time in ms
 
+# data containers
+ts, adc0, adc1 = [], [], []
 
-ts = []
-adc0 = []
-adc1 = []
-
-def animate(_, n, q, data):
-    if not q.empty():
+def animate(_, n, q):
+    while not q.empty():
         message = q.get_nowait()
         print('Got a message from GUI: ', message)
         q.task_done()
-    else:
-        message = None
-
-    # process all data on queue
-    while not data.empty():
-        line = data.get()
-        t, v0, v1 = line.split(',')
-        ts.append(float(t))
-        adc0.append(psi(float(v0)))
-        adc1.append(psi(float(v1)))
-        data.task_done()
-        # print("queue emptied")
 
     # plot last n datapoints
-    ts_window = ts[-n:]
     adc0_window = adc0[-n:]
     adc1_window = adc1[-n:]
+    # ts_window = ts[-n:]                               # moving, real time timestamps
+    ts_window = [i for i in range(len(adc0_window))]    # static timestams
     ax.clear()
     ax.plot(ts_window, adc0_window, label='adc0')
     ax.plot(ts_window, adc1_window, label='adc1')
     ax.set_title('Live Sensor Readings')
     ax.set_xlabel('Time (ms)')
     ax.set_ylabel('Pressure (psi)')
+    ax.set_ylim(-5, 45)
     ax.legend(loc='lower right')
     # fig.tight_layout()
 
 
 window_size = 100
-ani = animation.FuncAnimation(fig, animate, interval=200, fargs=(window_size, ui_queue, data_collection_queue))
+ani = animation.FuncAnimation(fig, animate, interval=update_rate_ms, fargs=(window_size, ui_queue))
 plt.draw()  # must call plot.draw() to start the animation
 
 # helper function to pass matplotlib backend to pySimpleGui canvas
@@ -138,21 +140,23 @@ window = sg.Window('Pressure Sensors', layout, finalize=True, element_justificat
 fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
 
 # start a thread that continuously collects data
-threading.Thread(target=data_collection_thread, args=(data_collection_queue,), daemon=True).start()
+threading.Thread(target=data_collection_thread, args=(data_collection_queue,True), daemon=True).start()
 
 # main event loop frr GUI
 while True:
 
-    event, values = window.read(timeout=500)
+    event, values = window.read(timeout=update_rate_ms)
 
     # check for exit
     if event in ('Exit', None):
         break
 
+    # send data to animation function
     ui_queue.put_nowait(event)
 
-    ts = ts[-window_size:]
-    adc0 = adc0[-window_size:]
-    adc1 = adc1[-window_size:]
+    # update data containers
+    print(len(ts), data_collection_queue.qsize())
+    ts, adc0, adc1 = process_data(data_collection_queue, ts, adc0, adc1)
+    print(len(ts), data_collection_queue.qsize(), ui_queue.qsize())
 
 window.close()
